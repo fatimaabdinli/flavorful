121e3d59533f9363692fa874e46e7fd7743d1ec7
fatimaabdinli <ms.fatima.abdinli@gmail.com>
2024-04-27 17:52:17 +0400

    created LogIn and SignUp UI

---
+++ flavorful/Business Layer/ViewModel/Log In/LoginVC.swift
@@ -0,0 +1,28 @@
+//
+//  LoginVC.swift
+//  flavorful
+//
+//  Created by Fatima Abdinli on 24.04.24.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    @IBOutlet weak var helloLabel: UILabel!
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passLabel: UILabel!
+    @IBOutlet weak var passTextfield: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var signUpLabel: UILabel!
+    @IBOutlet weak var signUpButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ flavorful/Business Layer/ViewModel/Sign Up/SignupVC.swift
@@ -0,0 +1,17 @@
+//
+//  SignupVC.swift
+//  flavorful
+//
+//  Created by Fatima Abdinli on 27.04.24.
+//
+
+import UIKit
+
+class SignupVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
